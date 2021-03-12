31d804af13f8d5d2abec7e1aa76b32db38251a6d
syl20ko <sylvain.caron@baliztic.com>
2021-03-13 00:17:47 +0100

    usestate from YTB Grafikart

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  const handleClick = function (e) {
+    e.preventDefault();
+    setCount(10);
+  };
+
+  return <button onClick={handleClick}>Nombre : {count}</button>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Counter />
+      <Counter />
+      <Counter />
     </div>
   );
 }
